2dc707a3cf24b236327614092d1dbf2aba1cdffa
kanchan kahar <kaharkanchan884@gmail.com>
2025-05-21 03:51:05 +0000

    data

---
+++ 01basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"  //treat all js  code as newer version
+
+let name = "kanchan";
+let age = 21;
+let isloggin = false;
+let state = null
+
+// in JS there two type of datatype;
+//1. premitive 
+//1.number = 3
+//2.string = "";
+//3.bigInt = 122323345n;
+//4.boolean = true/false;
+//5.null => standLONE vale
+//6.undefined
+//7.symbol= symbol("unique");
+
+//2.non-preemitive
+//1.array
+//2.object
+//3.function
+
+
+
+console.log(typeof "kanchan")
+console.log(typeof age)
+console.log(typeof null);  //object
+console.log(typeof undefined)   //undefined
