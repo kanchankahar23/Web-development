e4965a3d2a9326a7e83ed064a8b3793c91d7db51
kanchan kahar <kaharkanchan884@gmail.com>
2025-05-21 04:17:50 +0000

    conversion

---
+++ 01basic/03_conversion.js
@@ -0,0 +1,26 @@
+let score = "99";
+console.log(typeof score);
+console.log(typeof  (score) );
+let number = Number(score)
+// convert value;
+// "33" => 33
+// "33abc"=> NaN
+let isloggin = 1;
+let booleanisloggin = Boolean(isloggin)
+console.log(typeof booleanisloggin)
+
+
+let isName = "kanchan";
+let booleanisName = Boolean(isName)
+console.log(booleanisName)
+console.log(typeof booleanisName);
+
+//1=> true , 0 => false;
+//"" => false
+//"kanchan " => true
+
+
+let somenumber = 23;
+let stringnumber = String(somenumber);
+console.log(stringnumber)
+console.log(typeof stringnumber)
