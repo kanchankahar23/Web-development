242405d5e1288ee44908b9ff38b4c154a85bfea2
kanchan kahar <kaharkanchan884@gmail.com>
2025-07-19 14:32:08 +0530

    learn the different type of loop

---
+++ 04Loop/loop.js
@@ -0,0 +1,12 @@
+// Loops: there are 5 type of loop in javascript
+// 1.for
+// Q1 Print numbers from 1 to 10 using a for loop
+ for(let i = 1 ; i <= 10 ; i++){
+    console.log(i);
+ }
+
+
+// 2.while
+// 3.do...while
+// 4.for...of
+// 5.for...in
